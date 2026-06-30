bac9cb7def567e3d5e69800630a3fd1aff0ce6cf
pillow20 <108447638+pillow20@users.noreply.github.com>
2026-06-30 18:56:26 +0300

    Clean up comments in main.dart
    Removed comments about application entry point and dependencies.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,3 @@
-// Точка входа в приложение
-// Зависимости:
-// - flutter/material.dart (запуск Flutter-движка)
-// - app.dart (корневой виджет приложения)
-
 import 'package:flutter/material.dart';
 import 'app.dart';
 
